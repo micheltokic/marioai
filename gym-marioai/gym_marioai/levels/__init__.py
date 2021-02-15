53fdf79433e8087898dad29cdf871c9f2ef23d8e
Magdalena <Magdalena.Baumgaertl@web.de>
2021-02-15 18:34:52 +0100

    added other levels

--- gym-marioai/gym_marioai/levels/__init__.py
+++ gym-marioai/gym_marioai/levels/__init__.py
@@ -6,3 +6,6 @@ easy_level = __path + 'easyLevel.lvl'
 flat_level = __path + 'flatLevel.lvl'
 hard_level = __path + 'hardLevel.lvl'
 cliff_level = __path + 'cliffLevel.lvl'
+one_cliff_level = __path + 'oneCliffLevel.lvl'
+coin_level = __path + "coinLevel.lvl"
+enemy_level = __path + "enemyLevel.lvl"
