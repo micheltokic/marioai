136fe56f4814e19a11aeb5e7f29d1c81aa4fdb8e
Magdalena <Magdalena.Baumgaertl@web.de>
2021-02-22 11:43:48 +0100

    added early level

--- gym-marioai/gym_marioai/levels/__init__.py
+++ gym-marioai/gym_marioai/levels/__init__.py
@@ -9,3 +9,5 @@ cliff_level = __path + 'cliffLevel.lvl'
 one_cliff_level = __path + 'oneCliffLevel.lvl'
 coin_level = __path + "coinLevel.lvl"
 enemy_level = __path + "enemyLevel.lvl"
+early_cliff_level = __path + "earlyCliffLevel.lvl"
+none = "None"
