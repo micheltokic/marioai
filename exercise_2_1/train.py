4519b2ddc9abdd18713fb64b2dfebb6615cf6768
lex roodsar <a.vahid@campus.lmu.de>
2021-08-09 16:27:30 +0200

    added more trainingcycles -> (hopefully) better training results

--- exercise_2_1/train.py
+++ exercise_2_1/train.py
@@ -12,7 +12,7 @@ random_dataset = MDPDataset.load('exercise_2_1/data/random_data.h5')
 dqn = d3rlpy.algos.DQN()
 # train offline
 dqn.build_with_env(env)
-dqn.fit(random_dataset, n_epochs=1)
+dqn.fit(random_dataset, n_epochs=50)
 
 dqn.save_model('exercise_2_1/data/model.pt')
 
