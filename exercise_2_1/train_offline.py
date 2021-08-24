512cdbba54bdf358deca770b8ff562e407e6245e
lex roodsar <a.vahid@campus.lmu.de>
2021-08-24 10:05:10 +0200

    edited eps and gamma

--- exercise_2_1/train_offline.py
+++ exercise_2_1/train_offline.py
@@ -15,8 +15,8 @@ dataset = getDataset()
 train_episodes, test_episodes = train_test_split(dataset, test_size=0.2)
 
 # prepare algorithm
-opt = d3rlpy.models.optimizers.AdamFactory(optim_cls = 'Adam', betas = (0.9, 0.999), eps = 0.33, weight_decay = 0, asmgrad = False)
-dqn = d3rlpy.algos.DQN(learning_rate=3e-4, gamma=0.89, use_gpu=False, target_update_interval=5000) #target_update_interval anpassen epochenzahl* 6
+opt = d3rlpy.models.optimizers.AdamFactory(optim_cls = 'Adam', betas = (0.9, 0.999), eps = 0.1, weight_decay = 0, asmgrad = False)
+dqn = d3rlpy.algos.DQN(learning_rate=3e-4, gamma=0.7, use_gpu=False, target_update_interval=5000) #target_update_interval anpassen epochenzahl* 6
 #dqn = d3rlpy.algos.DoubleDQN(learning_rate=0.1, gamma=0.89, eps = 0.33)
 # train offline
 dqn.build_with_dataset(dataset)
