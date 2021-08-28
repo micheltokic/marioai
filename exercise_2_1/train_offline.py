dd12850fceb7e31ef85a33cdedf54db7bb523057
YStefan Koerner <git@skoerner.com>
2021-08-28 15:10:17 +0200

    add option for tensorboard and network options

--- exercise_2_1/train_offline.py
+++ exercise_2_1/train_offline.py
@@ -4,21 +4,24 @@ import d3rlpy
 from d3rlpy.metrics.scorer import evaluate_on_environment
 from sklearn.model_selection import train_test_split
 from gym_marioai import levels
+from d3rlpy.models.encoders import VectorEncoderFactory
+# encoder factory
+encoder_factory = VectorEncoderFactory(hidden_units=[256, 256])
 
-level = "levels/RoughTerrainLevel.lvl"
+level = "exercise_2_1/levels/RoughTerrainLevel.lvl"
 
 if __name__ == '__main__':
     env = Env(visible=False, port='8083', level=level, run_server=True).env
 
     dataset = getDataset()
 
-    train_episodes, test_episodes = train_test_split(dataset, test_size=0.2)
+    train_episodes, test_episodes = train_test_split(dataset, test_size=0.1)
 
     # prepare algorithm
     opt = d3rlpy.models.optimizers.AdamFactory(optim_cls='Adam', betas=(0.9, 0.999), eps=0.1, weight_decay=0,
                                                asmgrad=False)
-    dqn = d3rlpy.algos.DQN(learning_rate=3e-4, gamma=0.7, use_gpu=False,
-                           target_update_interval=5000)  # target_update_interval anpassen epochenzahl* 6
+    dqn = d3rlpy.algos.DQN(learning_rate=3e-4, gamma=0.7, use_gpu=True,
+                           target_update_interval=5000, encoder_factory=encoder_factory)  # target_update_interval anpassen epochenzahl* 6
     # dqn = d3rlpy.algos.DoubleDQN(learning_rate=0.1, gamma=0.89, eps = 0.33)
     # train offline
     dqn.build_with_dataset(dataset)
@@ -27,7 +30,7 @@ if __name__ == '__main__':
     # evaluate algorithm on the environment
     rewards = evaluate_scorer(dqn)
 
-    dqn.fit(train_episodes, eval_episodes=test_episodes, n_epochs=20, scorers={
+    dqn.fit(train_episodes, eval_episodes=test_episodes, tensorboard_dir='runs',  experiment_name='marioai', n_epochs=20, scorers={
         'environment': evaluate_scorer
     })
 
